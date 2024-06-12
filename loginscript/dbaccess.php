ca18ce7b92126efc21cf09fe725f13f702f902d7
andrei <andreinuca@gmail.com>
2024-06-12 20:47:33 +0100

    cloud

--- loginscript/dbaccess.php
+++ loginscript/dbaccess.php
@@ -1,22 +1,20 @@
 <?php
-class Database {
+class Database
+{
 
-  public function connect() {
-    
-    try {
-      
-      $username = "grubuy";
-      $password = "7O4@0XcY#9BXcYW*6jT%i!&5";
+      public function connect()
+      {
 
-      $conn = new PDO("mysql:host=localhost;dbname=grubuy", $username, $password);
-      
-      return $conn;
-    } 
-    catch(PDOException $e) {
-      echo "<br>Connection failed: " . $e->getMessage(). "<br/>";
-    }
-  }
+            try {
 
+                  $username = "CCPDBUser";
+                  $password = "CCPDBPassword";
+
+                  $conn = new PDO("mysql:host=10.0.1.4;dbname=grubuy", $username, $password);
+
+                  return $conn;
+            } catch (PDOException $e) {
+                  echo "<br>Connection failed: " . $e->getMessage() . "<br/>";
+            }
+      }
 }
-
-?>
