ea7346976af57b8303d8cc0970597fedf9a618cf
andrei <andreinuca@gmail.com>
2024-06-13 19:41:15 +0100

    g

--- register.php
+++ register.php
@@ -6,10 +6,10 @@
 
       <title>Grubuy - Register</title>
 
-      <link rel="shortcut icon" type="image/x-icon" href="/imagens/grubuy5.png" />
+      <link rel="shortcut icon" type="image/x-icon" href="imagens/grubuy5.png" />
 
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-      <link rel="stylesheet" href="/css/style.css">
+      <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
